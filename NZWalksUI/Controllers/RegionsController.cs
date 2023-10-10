7516ee847cfa35220fda5be2d2179ed6d47e1a19
Dileepa97 <54783169+Dileepa97@users.noreply.github.com>
2023-10-10 22:16:05 +0530

    add mvc web view to consume api

---
+++ NZWalksUI/Controllers/RegionsController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksUI.Models;
+using NZWalksUI.Models.DTO;
+using System.Text;
+using System.Text.Json;
+
+namespace NZWalksUI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+
+            try
+            {
+                //Get all regions
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponse = await client.GetAsync("https://localhost:7069/api/regions");
+
+                httpResponse.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponse.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7069/api/regions"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponse = await client.SendAsync(httpRequest);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Regions");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpResponse = await client.GetFromJsonAsync<RegionDto>($"https://localhost:7069/api/regions/{id.ToString()}");
+
+
+            if (httpResponse is not null)
+            {
+                return View(httpResponse);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7069/api/regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponse = await client.SendAsync(httpRequest);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Edit", "Regions");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponse = await client.DeleteAsync($"https://localhost:7069/api/regions/{request.Id}");
+
+                httpResponse.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return View("Edit");
+        }
+    }
+}
